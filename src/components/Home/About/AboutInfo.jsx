2f5dd0392b3ef9fe32f77f268076b9f0c5009ccf
islomnumanovuz <islomnumanov.uz@gmail.com>
2022-05-17 03:50:38 +0300

    "Updated with styles"

--- src/components/Home/About/AboutInfo.jsx
+++ src/components/Home/About/AboutInfo.jsx
@@ -5,6 +5,7 @@ import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import TelegramIcon from "@material-ui/icons/Telegram";
 import FacebookIcon from "@material-ui/icons/Facebook";
+import LinkIcon from "@material-ui/icons/Link";
 import Typewriter from "typewriter-effect";
 
 const AboutInfo = () => {
@@ -17,15 +18,15 @@ const AboutInfo = () => {
   }, []);
 
   return (
-    <div className="aboutInfo">
+    <div className="about-info">
       {portfolios.map((portfolio) => (
         <>
           <div className="side2">
-            <img src={portfolio.image} alt="" />
+            <img className="side2__image" src={portfolio.image} alt="images" />
           </div>
           <div className="side1">
-            <h1>
-              {portfolio.title}
+            <h1 className="about-info__title">
+              {portfolio.title + " "}
               <Typewriter
                 className="type-animation"
                 options={{
@@ -37,29 +38,76 @@ const AboutInfo = () => {
                 }}
               />
             </h1>
-            <p>{portfolio.description}</p>
+            <p className="about-info__description">{portfolio.description}</p>
+            <div className="button-group">
+              <Link className="linkBtn" to="/">
+                Read more
+              </Link>
+              <Link className="linkBtn" to="/">
+                <LinkIcon /> Portfolio
+              </Link>
+            </div>
+            <div className="socialBtn">
+              <a href="#" target="_blank">
+                <GitHubIcon />
+              </a>
+              <a href="#" target="_blank">
+                <LinkedInIcon />
+              </a>
+              <a href="#" target="_blank">
+                <TelegramIcon />
+              </a>
+              <a href="#" target="_blank">
+                <FacebookIcon />
+              </a>
+            </div>
           </div>
-          <div className="button-group">
-            <Link className="linkBtn" to="/">
-              Hey
-            </Link>
-            <Link className="linkBtn" to="/">
-              Hey
-            </Link>
-          </div>
-          <div className="socialBtn">
-            <a href="#" target="_blank">
-              <GitHubIcon />
-            </a>
-            <a href="#" target="_blank">
-              <LinkedInIcon />
-            </a>
-            <a href="#" target="_blank">
-              <TelegramIcon />
-            </a>
-            <a href="#" target="_blank">
-              <FacebookIcon />
-            </a>
+          <div className="effect-wrap">
+            <span className="effect effect-1"></span>
+            <span className="effect effect-2">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+            </span>
+            <span className="effect effect-3"></span>
+            <span className="effect effect-4"></span>
+            <span Name="effect effect-5 justify-content-between">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+            </span>
           </div>
         </>
       ))}
