85528aeeb19419e5fba505b0720968a862057407
islomnumanovuz <islomnumanov.uz@gmail.com>
2022-05-24 03:26:39 +0300

    Added responsive

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./components/styles/main.scss";
 import "./components/styles/animation.scss";
+import "./components/styles/rpw.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomeMain from "./components/01 Home/Main/HomeMain";
 import AboutInfo from "./components/02 About/AboutInfo";
